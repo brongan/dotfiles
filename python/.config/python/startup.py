17d8a7cbe3023b3033265bab8fa6ccdefb3e12f2
Brennan Tracy <brennantracy4@gmail.com>
2021-08-11 22:46:05 -0700

    Fix python histfile

--- python/.config/python/startup.py
+++ python/.config/python/startup.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+import atexit
 import os
+import readline
+from pathlib import Path
+
 
 if 'PYTHONHISTFILE' in os.environ:
-    history = os.path.expanduser(os.environ['PYTHONHISTFILE'])
+    histfile = Path(os.environ['PYTHONHISTFILE']).relative_to(Path.home())
 elif 'XDG_DATA_HOME' in os.environ:
-    history = os.path.join(os.path.expanduser(os.environ['XDG_DATA_HOME']),
-                           'python', 'python_history')
+    histfile = Path(os.environ['XDG_DATA_HOME']) / 'python' / 'python_history'
 else:
-    history = os.path.join(os.path.expanduser('~'),
-                           '.python_history')
-    history = os.path.abspath(history)
-_dir, _ = os.path.split(history)
-os.makedirs(_dir, exist_ok=True)
+    histfile = Path.home() / '.python_history'
+
+histfile.parent.mkdir(parents=True, exist_ok=True)
+
+try:
+    readline.read_history_file(histfile)
+except FileNotFoundError:
+    histfile.touch()
+
+readline.set_history_length(2000)
+atexit.register(readline.write_history_file, histfile)
 
