06b7640847fbe4017d772c528c22e3ce128367f1
Brennan Tracy <brennantracy4@gmail.com>
2022-02-02 10:12:54 -0800

    Make launch_polybar.sh idempotetish or something

--- scripts/.local/bin/launch_polybar.sh
+++ scripts/.local/bin/launch_polybar.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/env sh
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for m in $(xrandr --query -display ${DISPLAY}| grep " connected" | cut -d" " -f1); do
   MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top &
   MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini bottom &
