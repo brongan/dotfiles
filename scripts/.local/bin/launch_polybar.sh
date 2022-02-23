db060e0f72977a4d405bcb2646d04ae68b712e80
Brennan Tracy <brennantracy4@gmail.com>
2022-02-23 14:21:58 -0800

    Avoid automatically reloading polybar

--- scripts/.local/bin/launch_polybar.sh
+++ scripts/.local/bin/launch_polybar.sh
@@ -2,7 +2,7 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for m in $(xrandr --query -display ${DISPLAY}| grep " connected" | cut -d" " -f1); do
-  MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini top &
-  MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini bottom &
+  MONITOR=$m polybar -c ~/.config/polybar/config.ini top &
+  MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom &
 done
 
