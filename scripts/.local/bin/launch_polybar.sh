eb94f00dc02288692f01520471a05535eb26eacc
Brennan Tracy <brennantracy4@gmail.com>
2023-03-17 22:07:08 -0700

    Disable bottom polybar

--- scripts/.local/bin/launch_polybar.sh
+++ scripts/.local/bin/launch_polybar.sh
@@ -3,6 +3,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for m in $(xrandr --query -display ${DISPLAY}| grep " connected" | cut -d" " -f1); do
   MONITOR=$m polybar -c ~/.config/polybar/config.ini top &
-  MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom &
 done
 
