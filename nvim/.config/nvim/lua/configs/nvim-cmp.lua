ca0e1073923f3bb83927bc9361ef5c4335a1a0e3
Brennan Tracy <brennantracy4@gmail.com>
2024-01-09 22:28:57 -0800

    Remove cmp.complete().

--- nvim/.config/nvim/lua/configs/nvim-cmp.lua
+++ nvim/.config/nvim/lua/configs/nvim-cmp.lua
@@ -106,12 +106,8 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-				-- that way you will only jump inside the snippet region
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
 			else
 				fallback()
 			end
