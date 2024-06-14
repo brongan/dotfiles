3dbce182cdcd41c7e6c333c530cd2b44ef629907
Brennan Tracy <brennantracy@google.com>
2024-06-14 23:32:26 +0000

    I can't live without multi function tab competion.

--- nvim/.config/nvim/lua/configs/nvim-cmp.lua
+++ nvim/.config/nvim/lua/configs/nvim-cmp.lua
@@ -1,3 +1,5 @@
+vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
+
 return {
 	{
 		"saadparwaiz1/cmp_luasnip",
@@ -111,26 +113,33 @@ return {
 					['<C-n>'] = cmp.mapping.select_next_item(),
 					-- Select the [p]revious item
 					['<C-p>'] = cmp.mapping.select_prev_item(),
-
 					-- Scroll the documentation window [b]ack / [f]orward
 					['<C-b>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
-					['<C-y>'] = cmp.mapping.confirm { select = true },
-
-					-- If you prefer more traditional completion keymaps,
-					-- you can uncomment the following lines
-					--['<CR>'] = cmp.mapping.confirm { select = true },
-					--['<Tab>'] = cmp.mapping.select_next_item(),
-					--['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-					-- Manually trigger a completion from nvim-cmp.
-					--  Generally you don't need this, because nvim-cmp will display
-					--  completions whenever it has completion options available.
 					['<C-Space>'] = cmp.mapping.complete {},
+					['<C-y>'] = cmp.mapping.confirm { select = true },
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
 					-- Think of <c-l> as moving to the right of your snippet expansion.
 					--  So if you have a snippet that's like:
