701b70406339495f258daf08adc1b67e8c82de89
Brennan Tracy <brennantracy@google.com>
2024-06-17 23:27:27 +0000

    I don't want ruby neovim plz.

--- nvim/.config/nvim/lua/basics.lua
+++ nvim/.config/nvim/lua/basics.lua
@@ -24,6 +24,7 @@ vim.opt.swapfile = false
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 ---------------------------------------------------------------
 -- => General
