3d03f0592a69d86d7814807134dfc9daffa4a8b8
Brennan Tracy <brennantracy4@gmail.com>
2025-03-04 22:12:40 -0800

    remove polybar script.

--- scripts/.local/bin/launch_polybar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env sh
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-for m in $(xrandr --query -display ${DISPLAY}| grep " connected" | cut -d" " -f1); do
-  MONITOR=$m polybar -c ~/.config/polybar/config.ini top &
-done
-
