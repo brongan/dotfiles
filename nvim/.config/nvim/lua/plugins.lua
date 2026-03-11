87d652553a6f2092f3edca41fc58974ec319666f
Brennan Tracy <me@brongan.com>
2026-03-10 18:07:56 -0700

    markdown-preview.nvim -> render-markdown.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -124,11 +124,13 @@ local plugins = {
 
 	-- Markdown
 	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function() vim.fn["mkdp#util#install"]() end,
-	},
+		'MeanderingProgrammer/render-markdown.nvim',
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	}
+	,
 
 	-- Clipboard (OSC52)
 	{
