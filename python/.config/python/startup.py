8c833ee3ad2aca1085599f3d7cc35c0fb80e0143
Brennan Tracy <me@brongan.com>
2026-09-03 18:12:09 -0700

    python: use PYTHON_HISTORY for readline history
    Configure Python history through the environment and create its parent directory during startup.

--- python/.config/python/startup.py
+++ python/.config/python/startup.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
-import atexit
 import os
 import readline
 from pathlib import Path
 
 
-if 'PYTHONHISTFILE' in os.environ:
-    histfile = Path(os.environ['PYTHONHISTFILE']).relative_to(Path.home())
-elif 'XDG_DATA_HOME' in os.environ:
-    histfile = Path(os.environ['XDG_DATA_HOME']) / 'python' / 'python_history'
-else:
-    histfile = Path.home() / '.python_history'
-
-histfile.parent.mkdir(parents=True, exist_ok=True)
-
-try:
-    readline.read_history_file(histfile)
-except FileNotFoundError:
-    histfile.touch()
-
+if history := os.environ.get('PYTHON_HISTORY'):
+    Path(history).parent.mkdir(parents=True, exist_ok=True)
 readline.set_history_length(2000)
-atexit.register(readline.write_history_file, histfile)
-
